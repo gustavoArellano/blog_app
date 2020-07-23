d4e79cb667a3b07d03a33de3200a908c55db0c93
Gustavo Arellano <gustavo813arellano@gmail.com>
2020-07-23 10:19:16 -0700

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,62 +1,62 @@
-# class PostsController < ApplicationController
-#   # I can shorten the repetitive code by calling from private "find_post"
-#   before_action :find_post, only: [:show, :update, :edit, :destroy]
+class PostsController < ApplicationController
+  # I can shorten the repetitive code by calling from private "find_post"
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
 
-#   def index
-#     @posts = Post.all.order("created_at DESC")
-#   end
+  def index
+    @posts = Post.all.order("created_at DESC")
+  end
 
-#   def new
-#     #Want to create a new post item:
-#     @post = Post.new
-#   end
+  def new
+    #Want to create a new post item:
+    @post = Post.new
+  end
 
-#   def create
+  def create
 
-#     @post = Post.new(post_params)
+    @post = Post.new(post_params)
 
-#     if @post.save
-#       redirect_to @post
-#     else
-#       render 'new'
-#     end
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
 
-#   end
+  end
 
-#   def show 
-#     # @post = Post.find(params[:id])
-#   end
+  def show 
+    # @post = Post.find(params[:id])
+  end
 
-#   def update
-#     # @post = Post.find(params[:id])
+  def update
+    # @post = Post.find(params[:id])
 
-#     if @post.update(post_params)
-#       redirect_to @post
-#     else
-#       render 'edit'
-#     end
-#   end
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
-#   def edit
-#     # @post = Post.find(params[:id])
-#   end
+  def edit
+    # @post = Post.find(params[:id])
+  end
 
-#   def destroy 
-#     # @post = Post.find(params[:id])
-#     @post.destroy
+  def destroy 
+    # @post = Post.find(params[:id])
+    @post.destroy
 
-#     redirect_to posts_path
-#   end
+    redirect_to posts_path
+  end
 
 
-#   private
+  private
 
-#   def post_params
-#     params.require(:post).permit(:title, :content)
-#   end
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
 
-#   def find_post
-#     @post = Post.find(params[:id])
-#   end
+  def find_post
+    @post = Post.find(params[:id])
+  end
 
-# end
+end
