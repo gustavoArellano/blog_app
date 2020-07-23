04a1b7a49ccf101118e6027a64ba0e1e36e9d0bd
Gustavo Arellano <gustavo813arellano@gmail.com>
2020-07-23 01:46:24 -0700

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :find_post, only: [:show, :update, :edit, :destroy]
 
   def index
-    @posts = Post.all.order("created_at DESC")
+    @post = Post.all.order("created_at DESC")
   end
 
   def new
